dd1e214bf4f591914d3ef2801d44e54201879f94
Kiet Tran <charlieismylife2003@gmail.com>
2025-02-11 19:52:35 +0700

    fix firebase

--- src/common/utils/firebase.ts
+++ src/common/utils/firebase.ts
@@ -3,7 +3,11 @@ import admin from "firebase-admin"
 import * as serviceAccount from "./sobee-firebase-sa.json"
 
 const app = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
+  credential: admin.credential.cert({
+    projectId: serviceAccount.project_id,
+    clientEmail: serviceAccount.client_email,
+    privateKey: process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY!.replace(/\\n/g, "\n")
+  })
 })
 
 export const messaging = app.messaging()
